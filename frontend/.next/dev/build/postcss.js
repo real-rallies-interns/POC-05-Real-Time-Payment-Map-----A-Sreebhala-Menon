4871b9f217b3b70f95c344b8f8caa84b14e17ac4
sreebhala <asreebhala.2006@gmail.com>
2026-04-29 13:43:52 +0530

    Add files via upload

---
+++ frontend/.next/dev/build/postcss.js
@@ -0,0 +1,6 @@
+var R=require("./chunks/[turbopack]_runtime.js")("postcss.js")
+R.c("chunks/[turbopack-node]_transforms_postcss_ts_00twv_1._.js")
+R.c("chunks/[root-of-the-server]__0xf.e13._.js")
+R.m("[turbopack-node]/child_process/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/child_process/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/OneDrive/Desktop/AI/EV_charging/files/real time payment/files1/real-rails-frontend/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/child_process/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/child_process/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/OneDrive/Desktop/AI/EV_charging/files/real time payment/files1/real-rails-frontend/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/child_process/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
